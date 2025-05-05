b423943ed193e1bd9ed4bfaa23b42d89018baf88
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2025-05-04 23:19:17 -0300

    cria o codigo de barras e publica na fila

--- lab-azure-functions/fnGeradorBoletos/DIOFuncHttp.cs
+++ lab-azure-functions/fnGeradorBoletos/DIOFuncHttp.cs
@@ -6,6 +6,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Azure.Messaging.ServiceBus;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using BarcodeStandard;
 
 namespace fnGeradorBoletos
 {
@@ -16,6 +19,8 @@ namespace fnGeradorBoletos
         private static readonly string _serviceBusConnectionString = Environment.GetEnvironmentVariable("ServiceBusConnectionString");
         private static readonly string _queueName = "gerador-codigo-barras";
 
+        private static readonly ILogger _logger = new LoggerFactory().CreateLogger("DIOFuncHttp");
+
 
         [FunctionName("barcode-generate")]
         public static async Task<IActionResult> Run(
@@ -24,15 +29,70 @@ namespace fnGeradorBoletos
 
             try
             {
-                var resultObject = new
-                {
-                    barcode = "1234567890123456789012345678901234567890",
-                    valorOgirinal = 100.00,
-                    DataVencimento = DateTime.Now.AddDays(30).ToString("yyyy-MM-dd"),
-                    ImageBase64 = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAAB4CAYAAAD1j3x2AAAAOXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZSBDcmVhdGlvbiB0aW1lADIwMjMtMDktMjVUMjE6NTI6NTYtMDQ6MDAq7+5cAAAAJXRFWHRkYXRlIFRpbWUAMjAyMy0wOS0yNVQyMToyNToyNi0wNDowMK8r7gAAAABJRU5ErkJggg=="
-                };
 
-                await SendFileFallback(resultObject, _serviceBusConnectionString, _queueName);
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+                string valorOriginal = data?.valorOriginal;
+                string dataVencimento = data?.dataVencimento;
+
+                string barcodeData;
+                if (string.IsNullOrEmpty(valorOriginal) || string.IsNullOrEmpty(dataVencimento))
+                {
+                    return new BadRequestObjectResult("Valor original e data de vencimento são obrigatórios.");
+                }
+
+                // validar formato da data yyyy-MM-dd
+                if (!DateTime.TryParse(dataVencimento.ToString(), out DateTime vencimento))
+                {
+                    return new BadRequestObjectResult("Data de vencimento inválida. Formato esperado: yyyy-MM-dd.");
+                }
+
+                if (!DateTime.TryParseExact(dataVencimento.ToString(), "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out vencimento))
+                {
+                    return new BadRequestObjectResult("Data de vencimento inválida. Formato esperado: yyyy-MM-dd.");
+                }
+
+                string dateStr = vencimento.ToString("yyyyMMdd");
+
+                // Conversão do valor para centavos ate 8 dígitos
+
+                if (!Decimal.TryParse(valorOriginal, out decimal valorDecimal))
+                {
+                    return new BadRequestObjectResult("Valor original inválido.");
+                }
+
+                int valorCentavos = (int)(valorDecimal * 100);
+                string valorStr = valorCentavos.ToString("D8");
+
+                string bankCode = "006";
+                string baseCode = string.Concat(bankCode, valorStr, dateStr);
+
+                // preenchimento do barcode para 44 caracteres
+                barcodeData = bankCode.Length < 44 ? baseCode.PadRight(44, '0') : baseCode.Substring(0, 44);
+
+                _logger.LogInformation($"Barcode data: {barcodeData}");
+
+                Barcode barcode = new Barcode();
+
+                var skImage = barcode.Encode(BarcodeStandard.Type.Code128, barcodeData);
+                
+                using (var encodeData = skImage.Encode(SkiaSharp.SKEncodedImageFormat.Png, 100))
+                {
+                    var imageBytes = encodeData.ToArray();
+
+                    var base64Image = Convert.ToBase64String(imageBytes);
+
+                    var resultObject = new
+                    {
+                        barcode = baseCode,
+                        valorOriginal,
+                        DataVencimento = dataVencimento,
+                        ImageBase64 = base64Image,
+                    };
+
+                    await SendFileFallback(resultObject, _serviceBusConnectionString, _queueName);
+                }
+
             }
             catch (Exception ex)
             {
